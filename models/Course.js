64c93c7166f840ffaa48d9ecd37a9b074fbec84e
Stive AZ <aven0006@algonquinlive.com>
2021-03-05 18:32:39 -0500

    Complete requirement two - add models

---
+++ models/Course.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Student = require('./Student');
+const schema = new mongoose.Schema({
+  code: String,
+  title: String,
+  description: String,
+  url: String,
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }]
+});
+const Model = mongoose.model('Course', schema);
+
+module.exports = Model;
